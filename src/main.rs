65053f111a2ce008a7e101a65a3082b894875569
Anton Sidorov <antosid@gmail.com>
2024-02-27 09:09:52 +0000

    next: Bump version to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use tokio;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.1.0",
+    version = "v0.2.0",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "Scale down ECS cluster"
 )]
